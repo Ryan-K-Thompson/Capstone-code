32543add9ae075b4f3ccd6a02bc252305ad500b8
Ryan-K-Thompson <ryan.k.thompson71@gmail.com>
2022-07-23 17:19:34 +1000

    update

--- radical_oxygen_test.py
+++ radical_oxygen_test.py
@@ -7,6 +7,7 @@ Created on Mon Jul 18 11:35:24 2022
 
 import sys
 sys.path.append(r'C:\Users\patag\Desktop\Uni\2022\Capstone\Cantera\SDToolbox\Python3\sdtoolbox')
+sys.path.append(r'C:\Users\patag\Desktop\Uni\2022\Capstone\Capstone-code\real_gas_and_stagnation.py')
 sys.path.append(r'C:\Users\patag\Desktop\Uni\2022\Capstone\Capstone-code')
 from real_gas_shock_and_stagnation import shock_perfect_gas, shock_calorically_imperfect, stagnation_conditions_isentropic, freestream_to_shock_to_stagnation_condition_calorically_imperfect
 import cantera as ct
@@ -16,24 +17,34 @@ from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import contour, contourf
 
-
-
-
-
 gas = ct.ThermoPhase("airNASA9noions.cti")  # Import the thermodynamic data as NASA9 format. This data contains the chemical species CO2 CO C O O2 C2
 
-# Define constants
-gasconstant = 8.3144598 
 
-# arc chamber:
-gas.HPX = 7.0585*10**6,90*10**3,'CO2:1'
-# nozzle:
-gas.equilibrate('HP')
-# shock
-rho2, u2, p2, h2, e2, gas = shock_calorically_imperfect(gas=gas, u1=2850)
-# stagnation
-stagnation_conditions_isentropic(gas=gas, u2=u2)
+# def radical_oxygen_mass_fraction_PWKnozzleflow(gas, enthalpy_addition, composition, P1):
+#     gas = gas
+#     enthalpy_0 = gas.h
+#     print('enthalpy_0 = ' + str(enthalpy_0))
+#     print("initial gas enthalpy = " + str(enthalpy_0))
+#     enthalpy_1 = -8.9415*10**6 + enthalpy_addition#7.0585*10**6#enthalpy_0 + enthalpy_addition
+#     # arc chamber, enthalpy addition, constant pressure
+#     gas.HPX = enthalpy_1, P1, composition
+#     # nozzle, equilibriate, constant enthalpy and pressure
+#     gas.equilibrate('HP')
+    
+#     gas()
+#     # shock
+#     # rho2, u2, p2, h2, e2, gas = shock_calorically_imperfect(gas=gas, u1=2850)
+#     # stagnation
+#     # stagnation_conditions_isentropic(gas=gas, u2=u2)
+#     print(gas.species_names)
+#     index = gas.species_names.index("O")
+#     print(index)
+#     print(gas.Y)
+#     O_mass_fraction = gas.Y[index]
+#     print(O_mass_fraction)
 
-gas()
+radical_oxygen_mass_fraction_PWKnozzleflow(gas = gas, enthalpy_addition = 16*10**6, composition = "CO2:1", P1 = 90*1000) 
+  
+
 
 #gas.TPX = T1=, p1, 'CO2:1'
--- real_gas_shock_and_stagnation.py
+++ real_gas_shock_and_stagnation.py
@@ -147,8 +147,34 @@ def freestream_to_shock_to_stagnation_condition_calorically_imperfect(condition,
     rho2, u2, p2, h2, e2, gas = shock_calorically_imperfect(gas, u1)
     stagnation_conditions_isentropic(gas, u2)
     gas()
+  
+def radical_oxygen_mass_fraction_PWKnozzleflow(gas, enthalpy_addition, composition, P1):
+    gas = gas
+    enthalpy_0 = gas.h
+    print('enthalpy_0 = ' + str(enthalpy_0))
+    print("initial gas enthalpy = " + str(enthalpy_0))
+    enthalpy_1 = -8.9415*10**6 + enthalpy_addition#7.0585*10**6#enthalpy_0 + enthalpy_addition
+    # arc chamber, enthalpy addition, constant pressure
+    gas.HPX = enthalpy_1, P1, composition
+    # nozzle, equilibriate, constant enthalpy and pressure
+    gas.equilibrate('HP')
     
-freestream_to_shock_to_stagnation_condition_calorically_imperfect("PWK", gas)  
+    gas()
+    # shock
+    # rho2, u2, p2, h2, e2, gas = shock_calorically_imperfect(gas=gas, u1=2850)
+    # stagnation
+    # stagnation_conditions_isentropic(gas=gas, u2=u2)
+    print(gas.species_names)
+    index = gas.species_names.index("O")
+    print(index)
+    print(gas.Y)
+    O_mass_fraction = gas.Y[index]
+    print(O_mass_fraction)
+
+radical_oxygen_mass_fraction_PWKnozzleflow(gas = gas, enthalpy_addition = 16*10**6, composition = "CO2:1", P1 = 90*1000) 
+  
+    
+# freestream_to_shock_to_stagnation_condition_calorically_imperfect(condition = "PWK", gas = gas)  
 
 
 # =============================================================================
