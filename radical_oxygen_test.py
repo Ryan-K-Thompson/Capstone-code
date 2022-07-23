458bd75f8a5a992ae33a13f2e389c83f6faa9655
Ryan-K-Thompson <ryan.k.thompson71@gmail.com>
2022-07-23 18:27:41 +1000

    created visualisations of radical oxygen

--- radical_oxygen_test.py
+++ radical_oxygen_test.py
@@ -7,44 +7,106 @@ Created on Mon Jul 18 11:35:24 2022
 
 import sys
 sys.path.append(r'C:\Users\patag\Desktop\Uni\2022\Capstone\Cantera\SDToolbox\Python3\sdtoolbox')
-sys.path.append(r'C:\Users\patag\Desktop\Uni\2022\Capstone\Capstone-code\real_gas_and_stagnation.py')
 sys.path.append(r'C:\Users\patag\Desktop\Uni\2022\Capstone\Capstone-code')
-from real_gas_shock_and_stagnation import shock_perfect_gas, shock_calorically_imperfect, stagnation_conditions_isentropic, freestream_to_shock_to_stagnation_condition_calorically_imperfect
+from real_gas_shock_and_stagnation import radical_oxygen_mass_fraction_PWKnozzleflow
 import cantera as ct
 import numpy as np
-from scipy.optimize import fsolve
-from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import contour, contourf
 
 gas = ct.ThermoPhase("airNASA9noions.cti")  # Import the thermodynamic data as NASA9 format. This data contains the chemical species CO2 CO C O O2 C2
 
-
-# def radical_oxygen_mass_fraction_PWKnozzleflow(gas, enthalpy_addition, composition, P1):
-#     gas = gas
-#     enthalpy_0 = gas.h
-#     print('enthalpy_0 = ' + str(enthalpy_0))
-#     print("initial gas enthalpy = " + str(enthalpy_0))
-#     enthalpy_1 = -8.9415*10**6 + enthalpy_addition#7.0585*10**6#enthalpy_0 + enthalpy_addition
-#     # arc chamber, enthalpy addition, constant pressure
-#     gas.HPX = enthalpy_1, P1, composition
-#     # nozzle, equilibriate, constant enthalpy and pressure
-#     gas.equilibrate('HP')
-    
-#     gas()
-#     # shock
-#     # rho2, u2, p2, h2, e2, gas = shock_calorically_imperfect(gas=gas, u1=2850)
-#     # stagnation
-#     # stagnation_conditions_isentropic(gas=gas, u2=u2)
-#     print(gas.species_names)
-#     index = gas.species_names.index("O")
-#     print(index)
-#     print(gas.Y)
-#     O_mass_fraction = gas.Y[index]
-#     print(O_mass_fraction)
-
-radical_oxygen_mass_fraction_PWKnozzleflow(gas = gas, enthalpy_addition = 16*10**6, composition = "CO2:1", P1 = 90*1000) 
-  
+enthalpy_linspace = np.linspace(0,25*10**6,250)
+O2_MF = 0*enthalpy_linspace
+O_MF = 0*enthalpy_linspace
+N2_MF = 0*enthalpy_linspace
+N_MF = 0*enthalpy_linspace
+NO_MF = 0*enthalpy_linspace
+C_MF = 0*enthalpy_linspace
+C2_MF = 0*enthalpy_linspace
+CO_MF = 0*enthalpy_linspace
+CO2_MF = 0*enthalpy_linspace
+CN_MF = 0*enthalpy_linspace
 
 
-#gas.TPX = T1=, p1, 'CO2:1'
+
+count = 0
+for enthalpy in enthalpy_linspace:
+    MF = radical_oxygen_mass_fraction_PWKnozzleflow(gas = gas, enthalpy_addition = enthalpy, composition = "CO2:0.5, N2:0.5", P1 = 90*1000) 
+    O2_MF[count] = MF[0]
+    O_MF[count] = MF[1]
+    N2_MF[count] = MF[2]
+    N_MF[count] = MF[3]
+    NO_MF[count] = MF[4]
+    C_MF[count] = MF[5]
+    C2_MF[count] = MF[6]
+    CO_MF[count] = MF[7]
+    CO2_MF[count] = MF[8]
+    CN_MF[count] = MF[9]
+    count = count + 1
+
+plt.figure()
+plt.title("Mixing chamber gas composition" + "\n" + "Initial compostion (CO$_2$:90%, N$_2$:10%)")
+plt.xlabel("Arc chamber enthalpy addition (MJ)")
+plt.ylabel("Species mass fraction")
+plt.ylim(bottom = 0, top = 1)
+plt.xlim(left = 0, right = 25)
+enthalpy_linspace = enthalpy_linspace/(10**6)
+plt.plot(enthalpy_linspace, O2_MF)
+plt.plot(enthalpy_linspace, O_MF)
+plt.plot(enthalpy_linspace, N2_MF)
+plt.plot(enthalpy_linspace, N_MF)
+plt.plot(enthalpy_linspace, NO_MF)
+plt.plot(enthalpy_linspace, C_MF)
+plt.plot(enthalpy_linspace, C2_MF)
+plt.plot(enthalpy_linspace, CO_MF)
+plt.plot(enthalpy_linspace, CO2_MF)
+plt.plot(enthalpy_linspace, CN_MF)
+plt.legend(["$O_2$","$O$", "$N_2$", "$N$", "$NO$", "$C$", "$C_2$", "$CO$", "$CO_2$", "$CN$"], loc = "upper left")
+
+# enthalpy_linspace = np.linspace(0,25*10**6,250)
+# O2_MF = 0*enthalpy_linspace
+# O_MF = 0*enthalpy_linspace
+# N2_MF = 0*enthalpy_linspace
+# N_MF = 0*enthalpy_linspace
+# NO_MF = 0*enthalpy_linspace
+# C_MF = 0*enthalpy_linspace
+# C2_MF = 0*enthalpy_linspace
+# CO_MF = 0*enthalpy_linspace
+# CO2_MF = 0*enthalpy_linspace
+# CN_MF = 0*enthalpy_linspace
+
+# count = 0
+# for enthalpy in enthalpy_linspace:
+#     MF = radical_oxygen_mass_fraction_PWKnozzleflow(gas = gas, enthalpy_addition = enthalpy, composition = "CO2:0.95, N2:0.05", P1 = 90*1000) 
+#     O2_MF[count] = MF[0]
+#     O_MF[count] = MF[1]
+#     N2_MF[count] = MF[2]
+#     N_MF[count] = MF[3]
+#     NO_MF[count] = MF[4]
+#     C_MF[count] = MF[5]
+#     C2_MF[count] = MF[6]
+#     CO_MF[count] = MF[7]
+#     CO2_MF[count] = MF[8]
+#     CN_MF[count] = MF[9]
+#     count = count + 1
+
+# plt.figure()
+
+# plt.title("Mixing chamber gas composition"+"\n"+"Initial compostion (CO$_2$:95%, N$_2$:5%)", loc = 'center')
+# plt.xlabel("Arc chamber enthalpy addition (MJ)")
+# plt.ylabel("Species mass fraction")
+# plt.ylim(bottom = 0, top = 1)
+# plt.xlim(left = 0, right = 25)
+# enthalpy_linspace = enthalpy_linspace/(10**6)
+# plt.plot(enthalpy_linspace, O2_MF)
+# plt.plot(enthalpy_linspace, O_MF)
+# plt.plot(enthalpy_linspace, N2_MF)
+# plt.plot(enthalpy_linspace, N_MF)
+# plt.plot(enthalpy_linspace, NO_MF)
+# plt.plot(enthalpy_linspace, C_MF)
+# plt.plot(enthalpy_linspace, C2_MF)
+# plt.plot(enthalpy_linspace, CO_MF)
+# plt.plot(enthalpy_linspace, CO2_MF)
+# plt.plot(enthalpy_linspace, CN_MF)
+# plt.legend(["$O_2$","$O$", "$N_2$", "$N$", "$NO$", "$C$", "$C_2$", "$CO$", "$CO_2$", "$CN$"], loc = "upper left")
--- real_gas_shock_and_stagnation.py
+++ real_gas_shock_and_stagnation.py
@@ -151,25 +151,28 @@ def freestream_to_shock_to_stagnation_condition_calorically_imperfect(condition,
 def radical_oxygen_mass_fraction_PWKnozzleflow(gas, enthalpy_addition, composition, P1):
     gas = gas
     enthalpy_0 = gas.h
-    print('enthalpy_0 = ' + str(enthalpy_0))
-    print("initial gas enthalpy = " + str(enthalpy_0))
+    # print('enthalpy_0 = ' + str(enthalpy_0))
+    # print("initial gas enthalpy = " + str(enthalpy_0))
     enthalpy_1 = -8.9415*10**6 + enthalpy_addition#7.0585*10**6#enthalpy_0 + enthalpy_addition
     # arc chamber, enthalpy addition, constant pressure
     gas.HPX = enthalpy_1, P1, composition
     # nozzle, equilibriate, constant enthalpy and pressure
     gas.equilibrate('HP')
     
-    gas()
+    # gas()
     # shock
     # rho2, u2, p2, h2, e2, gas = shock_calorically_imperfect(gas=gas, u1=2850)
     # stagnation
     # stagnation_conditions_isentropic(gas=gas, u2=u2)
-    print(gas.species_names)
+    # print(gas.species_names)
     index = gas.species_names.index("O")
-    print(index)
-    print(gas.Y)
+    # print(index)
+    # print(gas.Y)
     O_mass_fraction = gas.Y[index]
-    print(O_mass_fraction)
+    mass_fractions = gas.Y
+    # print(O_mass_fraction)
+    print(mass_fractions)
+    return mass_fractions
 
 radical_oxygen_mass_fraction_PWKnozzleflow(gas = gas, enthalpy_addition = 16*10**6, composition = "CO2:1", P1 = 90*1000) 
   
